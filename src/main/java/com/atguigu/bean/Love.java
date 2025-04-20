3df759289f9f4f80ebc5ef9f61184538eb10688e
朱超 <2993824440@qq.com>
2025-04-20 18:30:00 +0800

    在idea中练习git使用

---
+++ src/main/java/com/atguigu/bean/Love.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author ZQ
+ * @create 2025-04-20-18:22
+ */
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("今天是什么日子...");
+    }
+}
