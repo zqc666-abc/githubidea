4bf56cdff5ad1ac2f631634fdb7572abda8e55f2
朱超 <2993824440@qq.com>
2025-04-20 18:51:28 +0800

    演示合并分支,产生冲突

--- src/main/java/com/atguigu/bean/Love.java
+++ src/main/java/com/atguigu/bean/Love.java
@@ -7,6 +7,6 @@ package com.atguigu.bean;
 public class Love {
     public static void main(String[] args) {
         System.out.println("今天是什么日子...");
-        System.out.println("第一天见面的日子...");
+        System.out.println("第一天见面的日子...宇哥,你好");
     }
 }
